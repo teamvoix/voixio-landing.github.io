e592ea1e49f529b5f9548773efe2db56ac793e87
Wave <morethanicloud@icloud.com>
2024-05-17 16:23:19 +0500

    Changed public path

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/voixio-landing.github.io',
       // analyze: true,
       // env: {},
       // rawDefine: {}
